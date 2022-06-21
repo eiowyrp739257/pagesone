5624c8da294d2ed7a936b9b0f5e38f71388aaf31
eiowyrp739257 <105581522+eiowyrp739257@users.noreply.github.com>
2022-06-21 10:19:09 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'nsbtehbrst.herokuapp.com'
+      url.hostname = 'xrkgvcesjcdr.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
